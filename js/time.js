c4b7b6e2a39ac2bc7c8e7c25f9af586220987254
João Oliveira <skreibar2pt@gmail.com>
2023-07-29 22:54:13 +0100

    updated function output

---
+++ js/time.js
@@ -0,0 +1,10 @@
+function updateTime() {
+            const now = new Date();
+            const hours = now.getHours().toString().padStart(2, '0');
+            const minutes = now.getMinutes().toString().padStart(2, '0');
+            const seconds = now.getSeconds().toString().padStart(2, '0');
+            const currentTime = `${hours}:${minutes}`;
+
+            // Update the content of the "time-container" div with the current time
+            document.getElementById('time-container').textContent = currentTime;
+        }
