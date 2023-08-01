6fe3e01775665b100f288b82134fd0bd6efb0af7
João Oliveira <skreibar2pt@gmail.com>
2023-08-01 18:46:41 +0100

    a

---
+++ js/footer.js
@@ -0,0 +1,5 @@
+// Get the current year using JavaScript
+const currentYear = new Date().getFullYear();
+
+// Set the current year in the HTML
+document.getElementById('currentYear').textContent = currentYear;
