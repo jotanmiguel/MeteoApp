8db5861d259c9a98ff9847bdc24710e6af7fc80c
João Oliveira <skreibar2pt@gmail.com>
2023-08-18 20:19:47 +0100

    a

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,35 +1,46 @@
+async function getCityName(lat, long) {
+    const apiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${long}`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const city = data.address.city || data.address.town || data.address.village || 'Unknown';
+    const country = data.address.country;
+    sessionStorage.setItem('location', JSON.stringify({
+        coords: {
+            lat: lat,
+            long: long
+        },
+        city: city,
+        country: country
+    }));
+    return { city: city, country: country };
+};
+
 if ('geolocation' in navigator) {
     // Create a map and set its initial view to a specific location and zoom level
     const myMap = L.map('map').setView([0, 0], 13);
 
-    navigator.geolocation.getCurrentPosition(function (position) {
+    navigator.geolocation.getCurrentPosition(async function (position) {
         var lat = position.coords.latitude;
         var long = position.coords.longitude;
 
-        const apiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${long}`;
+        try {
+            var locationInfo = await getCityName(lat, long);
+            const locationName = locationInfo.city + ', ' + locationInfo.country;
+            myMap.setView([lat, long], 13);
+            // Add a tile layer (you can use different tile layers, e.g., from OpenStreetMap)
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(myMap);
 
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                const city = data.address.city || data.address.town || data.address.village || 'Unknown';
-                locationName = city + ', ' + data.address.country;
-                myMap.setView([lat, long], 13);
-                // Add a tile layer (you can use different tile layers, e.g., from OpenStreetMap)
-                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(myMap);
+            // Create a marker with the given coordinates and add it to the map
+            let marker = L.marker([lat, long]).addTo(myMap);
 
-                // Create a marker with the given coordinates and add it to the map
-                let marker = L.marker([lat, long]).addTo(myMap);
-
-                // Optionally, you can add a popup to the marker
-                marker.bindPopup(locationName);
-
-            })
-            .catch(error => {
-                console.log('Error:', error);
-            });
+            // Optionally, you can add a popup to the marker
+            marker.bindPopup(locationName);
+        } catch (error) {
+            console.log(error);
+        }
     });
-
 } else {
     // If Geolocation API is not available, show an error message
     alert('Geolocation is not available in this browser.');
-}
+} 
--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,8 @@
+import { getCityName } from './geolocation.js';
 var key = "fda1c4c770e842d197b140037231204";
-var query = 'Porto Santo';
+var query = getCityName(33.0509055, -16.3581642).city;
 var weather;
+
 async function getWeatherInfo() {
     var apiUrl = 'http://api.weatherapi.com/v1/forecast.json?key=' + key + '&q=' + query + '&days=10&aqi=no';
     fetch(apiUrl)
@@ -10,9 +12,11 @@ async function getWeatherInfo() {
             document.getElementById('temp-container').innerText = weather.current.temp_c + 'ºC';
             const contentElement = document.getElementById('weather-main-box');
             contentElement.style.backgroundImage = 'url(' + weather.current.condition.icon + ')';
+            document.getElementById('weather-min-temp').innerText = 'Min. ' + weather.forecast.forecastday[0].day.mintemp_c + 'ºC';
+            document.getElementById('weather-max-temp').innerText = 'Max. ' + weather.forecast.forecastday[0].day.maxtemp_c + 'ºC';
+
         })
 };
 getWeatherInfo();
+setInterval(getWeatherInfo, 1000)
 console.log(weather);
-
-setInterval(getWeatherInfo, 1000*60*5)
